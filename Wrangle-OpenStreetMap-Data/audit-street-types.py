14d80e256d562610caaf29894a8e34494572336a
Rachel Fu <guanrong.fu@gmail.com>
2017-08-05 10:12:05 -0500

    docs: Add docs for this function
    Former-commit-id: dc1a3f25e9b3fd02699824c01c1503ae8620250e

--- Wrangle-OpenStreetMap-Data/audit-street-types.py
+++ Wrangle-OpenStreetMap-Data/audit-street-types.py
@@ -7,13 +7,23 @@ expected = ['Street', 'Avenue', 'Boulevard', 'Drive', 'Court', 'Place',
             'Square', 'Lane', 'Road', 'Trail', 'Parkway', 'Commons']
 
 
-def v_attrib_types(filename, k_value, pattern):
+def v_attrib_types(filename, k_attrib_value, pattern):
+    '''
+    This function will seperate the k attribute value based on their patterns
+    within a .osm file.
+
+    Parameters
+    ---
+    filename (.xml or .osm): The file that is going to be parsed.
+    k_attrib_value (str): The k attribute value you are looking for.
+    pattern (re): The regex pattern used to categorize the k attribute value
+    '''
     types = defaultdict(set)
 
     for event, element in ET.iterparse(filename, events=('start',)):
         if element.tag == 'node' or element.tag == 'way':
             for tag in element.iter('tag'):
-                if tag.attrib['k'] == k_value:
+                if tag.attrib['k'] == k_attrib_value:
                     v_value = tag.attrib['v']
                     search = pattern.search(v_value)
                     if search:
