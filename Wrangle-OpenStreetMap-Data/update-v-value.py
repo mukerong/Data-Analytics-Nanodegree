33d53556ae72f4fc7036e6a581f6c3c39201ef24
Rachel Fu <guanrong.fu@gmail.com>
2017-08-06 20:48:51 -0500

    refactor: Separate the functions into three
    The previous function cannot write to clean individual attributes.
    It is written to loop over all attributes stored in a dictionary.

--- Wrangle-OpenStreetMap-Data/update-v-value.py
+++ Wrangle-OpenStreetMap-Data/update-v-value.py
@@ -3,54 +3,24 @@ from collections import defaultdict
 import xml.etree.cElementTree as ET
 
 
-def v_attrib_types(filename, k_attrib_value, pattern, expected):
-    '''
-    This function will seperate the k attribute value based on their patterns
-    within a .osm file.
-
-    Parameters
-    ---
-    filename (.xml or .osm): The file that is going to be parsed.
-    k_attrib_value (str): The k attribute value you are looking for.
-    pattern (re): The regex pattern used to categorize the k attribute value
-    expected (list or re): the expected value list or pattern
-
-    Return
-    ---
-    A dictionary conatains the category of the k values.
-    '''
-    types = defaultdict(set)
-
-    for event, element in ET.iterparse(filename, events=('start',)):
-        if element.tag == 'node' or element.tag == 'way':
-            for tag in element.iter('tag'):
-                if tag.attrib['k'] == k_attrib_value:
-                    v_value = tag.attrib['v']
-                    search = pattern.search(v_value)
-                    if search:
-                        v_type = search.group(0)
-                        try:
-                            if v_type not in expected:
-                                types[v_type].add(v_value)
-                        except:
-                            if not expected.search(v_type):
-                                types[v_type].add(v_value)
-    return types
+def audit_street_type(street_types, street_name):
+    match = street_type_re.search(street_name)
+    if match:
+        street_type = m.group()
+        if street_type not in expected:
+            street_types[street_type].add(street_name)
 
 
-def update_street_value(types, mapping):
-    for st_types, names in types.items():
-        for name in names:
-            old_name = name.split(' ')[-1]
-            if old_name in mapping:
-                better_name = mapping[old_name]
-                name = name.replace(old_name, better_name)
-    return name
+def is_k_attrib(element, attrib_value):
+    return (element.attrib['k'] == attrib_value)
 
 
-def update_phone_value(types, pattern):
-    for phone_types, numbers in types.items():
-        for number in numbers:
-            if mapping.search(number):
-                number = re.sub(pattern, r'^\d\d\d-\d\d\d-\d\d\d\d$')
-    return number
+def audit(filename):
+    with open(filename, 'r') as f:
+        street_types = defaultdict(set)
+        for event, element in ET.iterparse(f, events=('start',)):
+            if element.tag == 'node' or elment.tag == 'way':
+                for tag in elment.iter('tag'):
+                    if is_k_attrib(tag):
+                        audit_street_type(street_types, tag.attrib['v'])
+    return street_types
