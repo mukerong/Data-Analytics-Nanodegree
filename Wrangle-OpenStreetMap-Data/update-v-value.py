d121a3e6aac5fc2ea06161ea5c55ebec6f870f1f
Rachel Fu <guanrong.fu@gmail.com>
2017-08-07 07:34:55 -0500

    feat: Update check_errors function
    This function can check again re patter now.

--- Wrangle-OpenStreetMap-Data/update-v-value.py
+++ Wrangle-OpenStreetMap-Data/update-v-value.py
@@ -8,8 +8,12 @@ def check_errors(v_value_types, v_value, pattern, expected):
     match = pattern.search(v_value)
     if match:
         v_value_type = match.group()
-        if v_value_type not in expected:
-            v_value_types[v_value_type].add(v_value)
+        try:
+            if v_value_type not in expected:
+                v_value_types[v_value_type].add(v_value)
+        except:
+            if not expected.search(v_value):
+                v_value_types[v_value_type].add(v_value)
     return v_value_types
 
 
@@ -28,6 +32,7 @@ def audit(filename, k_attrib_value, pattern, expected):
                         check_errors(v_value_types,
                                      tag.attrib['v'], pattern, expected)
     return v_value_types
+
 
 def fix_street_errors(error, mapping):
     update_name = error.split(' ')[-1]
