c8e9a5cbd6c4e0f5cb86e68133b397ed1e3442be
Rachel Fu <guanrong.fu@gmail.com>
2017-08-08 19:27:00 -0500

    feat: Read nodes.csv into database

---
+++ Wrangle-OpenStreetMap-Data/create-database.py
@@ -0,0 +1,39 @@
+# Import the modules
+import csv
+import sqlite3
+from pprint import pprint
+
+
+# Connect to the database and create a cursor object
+sqlite_file = 'osm_chicago.db'
+connect = sqlite3.connect(sqlite_file)
+cursor = connect.cursor()
+
+# Drop the table if exists
+cursor.execute('''
+               DROP TABLE IF EXISTS nodes
+''')
+
+# Create tables based on pre-defined schema
+cursor.execute('''
+        CREATE TABLE nodes (
+        id INTEGER PRIMARY KEY NOT NULL,
+        lat REAL,
+        lon REAL,
+        user TEXT,
+        uid INTEGER,
+        version INTEGER,
+        changeset INTEGER,
+        timestamp TEXT
+        );
+''')
+
+# Commit the change
+connect.commit()
+
+# Read the csv file as a dictionary, format the data as a list of tuples
+with open('nodes.csv', 'rb') as f:
+    dr = csv.DictReader(fin)
+    to_db = [(i['id'], i['lat'], i['lon'], i['user'],
+              i['uid'], i['version'], i['changeset'], i['timestamp'])
+             for i in dr]
