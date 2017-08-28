184aabf096cd5b5007dc2e819a602aa932e66fa1
Rachel Fu <guanrong.fu@gmail.com>
2017-08-28 08:28:37 -0500

    feat: Build up a decision tree algorithm

--- Machine-Learning/supporting-file/feature_selection/find_signature.py
+++ Machine-Learning/supporting-file/feature_selection/find_signature.py
@@ -8,7 +8,7 @@ numpy.random.seed(42)
 ### The words (features) and authors (labels), already largely processed.
 ### These files should have been created from the previous (Lesson 10)
 ### mini-project.
-words_file = "../text_learning/your_word_data.pkl" 
+words_file = "../text_learning/your_word_data.pkl"
 authors_file = "../text_learning/your_email_authors.pkl"
 word_data = pickle.load( open(words_file, "r"))
 authors = pickle.load( open(authors_file, "r") )
@@ -38,6 +38,11 @@ labels_train   = labels_train[:150]
 
 
 ### your code goes here
+from sklearn import tree
+from sklearn.metrics import accuracy_score
 
+clf = tree.DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+predict = clf.predict(features_test)
 
-
+print accuracy_score(predict, labels_test)
