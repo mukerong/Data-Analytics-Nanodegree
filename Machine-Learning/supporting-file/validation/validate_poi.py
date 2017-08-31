d86644d3416ef4197fa3da694707417e66e6cd1f
Rachel Fu <guanrong.fu@gmail.com>
2017-08-31 07:45:42 -0500

    feat: build train_test_split validation

--- Machine-Learning/supporting-file/validation/validate_poi.py
+++ Machine-Learning/supporting-file/validation/validate_poi.py
@@ -25,7 +25,11 @@ features_list = ["poi", "salary"]
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
+from sklearn.model_selection import train_test_split
 
+features_train, features_test, labels_train, labels_test = train_test_split(
+    features, labels, test_size=0.3, random_state=42
+)
 
 
 ### it's all yours from here forward!
