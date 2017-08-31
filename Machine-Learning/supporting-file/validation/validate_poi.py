1c69714b1a4ad4ec7877d60c5ebb451b3a1a1369
Rachel Fu <guanrong.fu@gmail.com>
2017-08-31 07:46:48 -0500

    feat: update the tree clacification

--- Machine-Learning/supporting-file/validation/validate_poi.py
+++ Machine-Learning/supporting-file/validation/validate_poi.py
@@ -37,6 +37,6 @@ from sklearn import tree
 from sklearn.metrics import accuracy_score
 
 tree = tree.DecisionTreeClassifier()
-tree.fit(features, labels)
-predict = tree.predict(features)
-accuracy_score(predict, labels)
+tree.fit(features_train, labels_train)
+predict = tree.predict(features_test)
+accuracy_score(predict, labels_test)
