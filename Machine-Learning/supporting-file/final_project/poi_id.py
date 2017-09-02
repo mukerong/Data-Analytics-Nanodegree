73ccf32648ff8196b0b2991c68e69330db3f253e
Rachel Fu <guanrong.fu@gmail.com>
2017-09-02 17:16:12 -0500

    fix: Update the module name

--- Machine-Learning/supporting-file/final_project/poi_id.py
+++ Machine-Learning/supporting-file/final_project/poi_id.py
@@ -17,7 +17,6 @@ with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
 
 ### Task 2: Remove outliers
-data_dict.pop('TOTAL', 0)
 ### Task 3: Create new feature(s)
 ### Store to enron_dataset for easy export below.
 enron_dataset = data_dict
@@ -44,7 +43,7 @@ clf = GaussianNB()
 ### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
 
 # Example starting point. Try investigating other evaluation techniques!
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 features_train, features_test, labels_train, labels_test = \
     train_test_split(features, labels, test_size=0.3, random_state=42)
 
