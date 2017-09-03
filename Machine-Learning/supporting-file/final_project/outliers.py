db03373d1ee9977d26a9343b14a063bce485c03c
Rachel Fu <guanrong.fu@gmail.com>
2017-09-02 19:41:19 -0500

    feat: Load the data into Pandas dataframe

--- Machine-Learning/supporting-file/final_project/outliers.py
+++ Machine-Learning/supporting-file/final_project/outliers.py
@@ -1,24 +1,24 @@
 import sys
+sys.path.append("../tools/")
 import pickle
+import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
+from pandas.plotting import scatter_matrix
 from feature_format import featureFormat, targetFeatureSplit
 
 # Read the original dataset
 with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
 
-# Remove outliers
-
+# Remove outliers and create a new data dictionary
 enron_dataset = data_dict
+
 # Read data using the defined feature list
 features_list = ['poi', 'salary']
 data = featureFormat(enron_dataset, features_list, sort_keys=True)
 
 # Visualize the data to find outliers
-for point in data:
-    poi = point[0]
-    salary = point[1]
-    plt.scatter(poi, salary)
-
-plt.xlabel('poi')
-plt.ylabel('salary')
+enron_dataframe = pd.DataFrame(enron_dataset.values())
+employees = pd.Series(enron_dataset.keys())
+enron_dataframe.set_index(employees, inplace=True)
