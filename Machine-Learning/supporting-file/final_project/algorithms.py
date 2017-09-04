e534ce154b41fcc31de409583396f08212409a20
Rachel Fu <guanrong.fu@gmail.com>
2017-09-04 18:31:22 -0500

    feat: Build SVM classifier

--- Machine-Learning/supporting-file/final_project/algorithms.py
+++ Machine-Learning/supporting-file/final_project/algorithms.py
@@ -7,6 +7,7 @@ from sklearn.feature_selection import SelectKBest, SelectPercentile
 from sklearn.model_selection import train_test_split, KFold, GridSearchCV
 from sklearn.metrics import accuracy_score, precision_score, recall_score
 from sklearn import tree
+from sklearn import
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.svm import SVC
 from sklearn.pipeline import Pipeline
@@ -23,15 +24,13 @@ features_train, features_test, labels_train, labels_test = train_test_split(
     features, labels, test_size=0.2, random_state=1
 )
 
-
-# Use pipeline to run multi-stage operations
+'''
+# Decision Tree
 scaler = MinMaxScaler()
 scaler.fit(features_train)
 scaled_features_train = scaler.transform(features_train)
 scaled_features_test = scaler.transform(features_test)
 
-'''
-# Decision Tree
 parameters = {'min_samples_split': (2, 5, 10)}
 tree = GridSearchCV(tree.DecisionTreeClassifier(), parameters)
 tree.fit(scaled_features_train, labels_train)
@@ -44,12 +43,24 @@ print "decision treere call_score", \
     recall_score(labels_test, tree_prediction)
 '''
 
-# SVM
-parameters = {'kernel': ('linear', 'rbf'), 'C': (1, 10)}
-svc = SVC()
-svc = GridSearchCV(svc, parameters)
-svc.fit(scaled_features_train, labels_train)
-svc_prediction = svc.predict(scaled_features_test)
+'''
+# Use pipeline to tune SVM
+estimators =[('scaler', MinMaxScaler()),
+             ('feature_selection', SelectKBest()),
+             ('svm', SVC())
+             ]
+pipeline = Pipeline(estimators)
+
+parameters = {
+    'feature_selection__k': [2, 3, 4, 5, 6, 'all'],
+    'svm__kernel': ['linear', 'rbf'],
+    'svm__C': [0.1, 1, 10, 100, 1000, 10000],
+    'svm__gamma': [0.1, 0.01, 0.001, 0.0001]
+}
+
+clf = GridSearchCV(pipeline, param_grid=parameters, scoring='f1',error_score=0)
+clf.fit(features_train, labels_train)
+svc_prediction = clf.predict(features_test)
 
 print "svc accuracy score: ", \
     accuracy_score(labels_test, svc_prediction)
@@ -57,3 +68,11 @@ print "svc precision_score: ", \
     precision_score(labels_test, svc_prediction)
 print "svc recall_score", \
     recall_score(labels_test, svc_prediction)
+'''
+
+
+# Use pipeline to tune KNN
+estimators = [('scaler', MinMaxScaler()),
+              ('feature_selection', SelectKBest()),
+              ('knn', )
+              ]
