12cc5eb7eed54d0df248e05313d6c6ddf0ceaed5
Rachel Fu <guanrong.fu@gmail.com>
2017-10-05 21:20:00 -0500

    chore: Re-organize the code

--- Machine-Learning/supporting-file/final_project/outliers.py
+++ Machine-Learning/supporting-file/final_project/outliers.py
@@ -10,8 +10,6 @@ from feature_format import featureFormat, targetFeatureSplit
 with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
 
-# Remove outliers and create a new data dictionary
-data_dict.pop('TOTAL', 0)
 
 # Read the data to a dataframe
 enron_dataframe = pd.DataFrame(data_dict.values())
@@ -24,6 +22,12 @@ enron_dataframe = enron_dataframe.apply(lambda x:
 poi = pd.Series([0, 1], index=[False, True])
 enron_dataframe['poi'] = enron_dataframe.poi.map(poi)
 
-# Visualize the data to find outliers
+# Draw the scatter plot with salary and poi to see if there are any outliers
+enron_dataframe.plot.scatter(x='poi', y='salary')
+
+# Remove Outliers
+data_dict.pop('TOTAL', 0)
+
+# Visualize the rest of the data to find outliers
 for col in enron_dataframe.columns:
     enron_dataframe.plot.scatter(x='poi', y=col)
